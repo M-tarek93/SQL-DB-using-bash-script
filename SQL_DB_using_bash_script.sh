98b6aefe87535d3866d02c9ee25372e668df783b
M-tarek93 <52703729+M-tarek93@users.noreply.github.com>
2020-02-02 22:39:21 +0200

    Updating the script
    adding tables operations (create,show,drop)
    changing prompt dynamically to match the current used database
    checking if the file or directory exists before creation

--- SQL_DB_using_bash_script.sh
+++ SQL_DB_using_bash_script.sh
@@ -1,13 +1,17 @@
 #!/user/bin/bash
 
 sudo test -d /var/lib/SQL_Bash/ || sudo mkdir /var/lib/SQL_Bash;
+sudo test -d /var/lib/SQL_Bash/Default_DB || sudo mkdir /var/lib/SQL_Bash/Default_DB;
 echo "Welcome to SQL DBMS using Bash Script";
-
+current_database="Default_DB";
 function SHOW
 {
 	case $1 in
 	DATABASES)
 		ls /var/lib/SQL_Bash;
+		;;
+	TABLES)
+		ls /var/lib/SQL_Bash/$current_database;
 		;;
 	esac
 }
@@ -15,7 +19,21 @@ function CREATE
 {
 	case $1 in
 	DATABASE)
+		if test -d /var/lib/SQL_Bash/$2
+		then
+		echo "Database already exists";
+		else
 		sudo mkdir -p /var/lib/SQL_Bash/$2;
+		fi
+		;;
+	TABLE)
+		if test -f /var/lib/SQL_Bash/$current_database/$2
+		then
+		echo "Table already exists";
+		else
+		sudo touch /var/lib/SQL_Bash/$current_database/$2;
+		sudo touch /var/lib/SQL_Bash/$current_database/.$2meta
+		fi		
 		;;
 	esac
 }
@@ -25,10 +43,25 @@ function DROP
 	DATABASE)
 		sudo rm -r /var/lib/SQL_Bash/$2;
 		;;
+	TABLE)
+		sudo rm /var/lib/SQL_Bash/$current_database/$2;
+		sudo rm /var/lib/SQL_Bash/$current_database/.$2meta;
+		;;
+	esac
+}
+function USE
+{
+	case $1 in
+	DATABASE)
+		
+		cd /var/lib/SQL_Bash/$2;
+		current_database=$2;
+		;;
+		
 	esac
 }
 while true
 do
-read statement;
+read -p "$current_database > " statement;
 $statement;
 done
