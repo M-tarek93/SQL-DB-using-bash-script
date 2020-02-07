009c8305165e66e2ebebf65f67b82e7d60645055
M-tarek93 <52703729+M-tarek93@users.noreply.github.com>
2020-02-07 15:09:30 +0200

    adding "insert into" function and editing "create table" function

--- SQL_DB_using_bash_script.sh
+++ SQL_DB_using_bash_script.sh
@@ -1,5 +1,4 @@
 #!/user/bin/bash
-
 sudo test -d /var/lib/SQL_Bash/ || sudo mkdir /var/lib/SQL_Bash;
 sudo test -d /var/lib/SQL_Bash/Default_DB || sudo mkdir /var/lib/SQL_Bash/Default_DB;
 echo "Welcome to SQL DBMS using Bash Script";
@@ -31,63 +30,56 @@ function CREATE
 		then
 		echo "Table already exists";
 		else
-		sudo touch /var/lib/SQL_Bash/$current_database/$2;
-		sudo touch /var/lib/SQL_Bash/$current_database/.$2meta
+		Create_Table $2;
 		fi		
 		;;
 	esac
 }
 function Create_Table
 {
-
-echo "please enter the table name";
-read tableName
-sudo touch /var/lib/SQL_Bash/$current_database/$tableName
+sudo touch /var/lib/SQL_Bash/$current_database/$1
+sudo touch /var/lib/SQL_Bash/$current_database/.${1}meta
 echo "How many columns do you want to enter?"
 read columnNumber
 for((i=0;i<$columnNumber;i++))
 do
-
-#read columnDataType
 echo "please enter the name of the column"
 read columnName
 echo "please enter the dataType of the column int/text"
 select columnDataType in int text
 do
 case $columnDataType in
-int)
-	sudo touch /var/lib/SQL_Bash/$current_database/.${tableName}meta	
-	sudo chmod +x .${tableName}meta
-	sudo echo $columnName"int" >> /var/lib/SQL_Bash/$current_database/.${tableName}meta
-	
-	
+int)	
+	echo "$columnName,"int"" | sudo tee -a /var/lib/SQL_Bash/$current_database/.${1}meta
+	break;
 	;;
 text)
-	sudo touch /var/lib/SQL_Bash/$current_database/.${tableName}meta	
-	sudo chmod +x .${tableName}meta	
-	sudo echo $columnName"text" >> /var/lib/SQL_Bash/$current_database/.${tableName}meta
-	
-	;;  		
+	echo "$columnName,"text"" | sudo tee -a /var/lib/SQL_Bash/$current_database/.${1}meta
+	break;
+	;;  
+*)
+	echo "Please choose int or text only"		
 esac
 done
-done	
+done
+echo "Table $tableName created successfully"
 }
+
 function Select
 {
-columnNumber="please enter the column's number"
-echo $columnNumber
-read columnNumber
+echo "please enter the column's number";
+read columnNumber;
 if [[ -v $columnNumber ]]
 then
-    cat -n$columnNumber /var/lib/SQL_Bash/$current_database/.${tableName}meta
+    cat -n$columnNumber /var/lib/SQL_Bash/$current_database/.${tableName}meta;
 else
-    echo "the column does not exist!"
+    echo "the column does not exist!";
+fi
 }
 
 function Delete
 {
-rowNumber="please enter row number"
-echo $rowNumber
+echo "please enter row number";
 read rowNumber
 sed -O '$rowNumber d'  /var/lib/SQL_Bash/$current_database/.${tableName}meta
 }
@@ -114,6 +106,80 @@ function USE
 		;;
 		
 	esac
+}
+
+function INSERT
+{
+case $1 in
+INTO)
+	case $3 in
+	VALUES)
+		if [ -f /var/lib/SQL_Bash/$current_database/.$2meta ]
+		then
+		table_lines_count=$(wc -l < /var/lib/SQL_Bash/$current_database/.$2meta);
+		column_types=($(cut -f2 -d, /var/lib/SQL_Bash/$current_database/.$2meta));
+		counter=0;
+		new_line="";
+		if (( "($#-3)" <= "$table_lines_count"))
+		then
+		for item in ${@:4}
+		do
+		case $item in
+		+([0-9]) )
+			if  [ "${column_types[$counter]}" = "int" ]
+			then
+				if [ -z "$new_line" ]
+				then
+				new_line=$item
+				else
+				new_line=$new_line","$item;
+				fi
+			else
+				echo "wrong type: $item should be text";
+				new_line="";
+				break;
+			fi
+			counter=$counter+1;
+			;;
+		+([a-zA-Z0-9_]) )
+			if [ ${column_types[$counter]} = "text" ]
+			then
+				if [ -z "$new_line" ]
+				then
+				new_line=$item
+				else
+				new_line=$new_line","$item;
+				fi
+			else
+				echo "wrong type: $item should be integer";
+				new_line="";
+				break;
+			fi
+			counter=$counter+1;
+			;;
+		esac		
+		done
+		else
+			echo "values count is more than the number of columns"
+		fi
+		if [ -n "$new_line" ]
+		then
+		echo "Values inserted successfully"
+		echo "$new_line" | sudo tee -a /var/lib/SQL_Bash/$current_database/$2
+		fi
+		else
+			echo "table not found";
+		fi
+		;;
+	*)
+		echo "Please use the following syntax: INSERT INTO (table name) VALUES (value1) (value2) ..";
+		;;
+	esac
+	;;
+*)
+	echo "Please use the following syntax: INSERT INTO (table name) VALUES (value1) (value2) ..";
+	;;
+esac
 }
 while true
 do
